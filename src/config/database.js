022cbe50eea9c46e0bdcc61158c11785765c62c9
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2022-01-14 13:54:02 -0300

    Testando a API (Postman)

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,11 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://localhost/mymoney',{useMongoClient:true})
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório"
+mongoose.Error.messages.Number.min =
+        "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
+mongoose.Error.messages.Number.max =
+        "O '{VALUE}' informado é maior que  o limite máximode '{MAX}'."
+mongoose.Error.messages.String.enum = 
+        "O '{VALUE}' não é válido para o atributo '{PATH}'."
