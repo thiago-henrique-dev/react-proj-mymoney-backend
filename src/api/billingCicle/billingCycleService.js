be886eaf087b6f7c0a2fa46fb7c5e6a639498571
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2022-01-14 14:03:13 -0300

    Obtendo a Qtde de Registros (Count)

--- src/api/billingCicle/billingCycleService.js
+++ src/api/billingCicle/billingCycleService.js
@@ -2,6 +2,16 @@ const BillingCycle = require('./billingCycle')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({new: true, runValidators: true})
+BillingCycle.route('count', (req, res, next) =>  {
+    BillingCycle.count((error, value) => {
+        if(error) {
+            res.status(500).json({errors: [error]})
+        } else {
+            res.json({value})
+        }
+
+    })
+})
 
 BillingCycle.route('get', (req, res,next) => {
     BillingCycle.find({}, (err, docs) => {
