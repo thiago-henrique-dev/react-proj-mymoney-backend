115ffb174271a15d2274299222e206a67966fbad
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2022-01-19 14:34:26 -0300

    Aplicação Final - Autenticação (Backend) - PROJECT FINISHED

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,12 +1,21 @@
 const express = require('express')
-
+const auth = require('./auth')
 module.exports = function (server) {
-
-    //Definir URL BASE
-    const router = express.Router()
-    server.use('/api', router)
-
-    //Rotas de Ciclo de Pagamento
-    const BillingCycle = require('../api/billingCicle/billingCycleService')
-    BillingCycle.register(router, '/billingCycles')
+    /*
+    * Rotas protegidas por Token JWT
+    */
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+    protectedApi.use(auth)
+    const BillingCycle = require('../api/billingCycle/bilingCycleService')
+    BillingCycle.register(protectedApi, '/billingCycles')
+    /*
+    * Rotas abertas
+    */
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+    const AuthService = require('../api/user/authService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
 }
--- src/config/server.js
+++ src/config/server.js
@@ -1,18 +1,18 @@
 const port = 3003
 
-const bodyParser = require('body-parser') // vai fazer o parse no corpo da requisição para entregar o objeto pronto
-const express = require('express') // require do node
-const server = express() // representa meu server
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
 const allowCors = require('./cors')
 const queryParser = require('express-query-int')
 
-server.use(bodyParser.urlencoded({ extended: true}))
+server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(allowCors)
 server.use(queryParser())
+server.listen(port, function () {
+    console.log(`Servidor Rondando na Porta ${port}`);
 
-server.listen(port, function() {
-    console.log(`BACKEND is running on port ${port}`)
 })
 
 module.exports = server
